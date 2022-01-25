af38aa4220dda0a111bc59aaa488f8b04169562c
Lrows <69643497+lrowsxrd@users.noreply.github.com>
2022-01-25 14:13:45 +0300

    Add files via upload

---
+++ emoji.js
@@ -0,0 +1,16 @@
+module.exports = {
+    onayemoji: "white_check_mark",
+     redemoj: "red_square", 
+     lrows:":tada:",
+     sıfır:"",
+     bir:"",
+     iki:"",
+     üc:"",
+     dört:"",
+     beş:"",
+     altı:"",
+     yedi:"",
+     sekiz:"",
+     dokuz:""
+  
+}
